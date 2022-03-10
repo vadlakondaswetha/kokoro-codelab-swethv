ddc9454d15fd4a97e98512cec8bfa66a53ab6181
Swetha Vadlakonda <swethv@googlers.com>
2022-03-10 21:02:11 +0530

    Kokoro codelab prepwork

---
+++ Hello.java
@@ -0,0 +1,14 @@
+package ;
+
+import com.google.errorprone.annotations.CheckReturnValue;
+
+/**
+ * 
+ */
+
+/** Simple java file used for Kokoro QA validation. */
+public class Hello {
+  public static void main(String[] argv) {
+    System.out.println("Hello");
+  }
+}
